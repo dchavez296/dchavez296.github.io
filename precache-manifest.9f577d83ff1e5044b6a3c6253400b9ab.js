439cc9ce4841358c696fdbdfca0d5c5a55227a1a
David Chavez Armenteros <32719242+N1GHTMAR329@users.noreply.github.com>
2023-03-29 17:45:08 -0400

    initial

---
+++ precache-manifest.9f577d83ff1e5044b6a3c6253400b9ab.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c19b4b880c262507c3acb473c730fa4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a9af54f4abebf4a3ccb",
+    "url": "/static/css/main.11cc2903.chunk.css"
+  },
+  {
+    "revision": "738cf3320eda408117f4",
+    "url": "/static/js/2.5280fab8.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.5280fab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a9af54f4abebf4a3ccb",
+    "url": "/static/js/main.9d07d889.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "9388d6146b3f9ee4678dfadeddc22c51",
+    "url": "/static/media/8secondz.9388d614.png"
+  },
+  {
+    "revision": "de8b4f8cd3cb78ecc22bb4280c3b0c2d",
+    "url": "/static/media/Solstar_logo.de8b4f8c.png"
+  },
+  {
+    "revision": "c7b2dbe09bb2846e7a55189bf9e51233",
+    "url": "/static/media/bartini.c7b2dbe0.png"
+  },
+  {
+    "revision": "7feddacba0a519c07a32a37f3f27c9c9",
+    "url": "/static/media/cabbagetreelandscape.7feddacb.png"
+  },
+  {
+    "revision": "ad8ee816190aba472e8bb754fc080d11",
+    "url": "/static/media/cactus.ad8ee816.png"
+  },
+  {
+    "revision": "9ef3cb7262237d548954ce357584c36f",
+    "url": "/static/media/cactuscan.9ef3cb72.png"
+  },
+  {
+    "revision": "b5e7976a07bc00e0998347d2224f43d3",
+    "url": "/static/media/greenrune.b5e7976a.png"
+  },
+  {
+    "revision": "ff892d71194730672450f790fa7e28d5",
+    "url": "/static/media/mail.ff892d71.png"
+  },
+  {
+    "revision": "f7cd36dc88b6ca3f8d50e7f1daec3e99",
+    "url": "/static/media/profile.f7cd36dc.png"
+  },
+  {
+    "revision": "9c1952ca5232cf092e46425369fa3111",
+    "url": "/static/media/psykinetic.9c1952ca.png"
+  },
+  {
+    "revision": "952556c1ac9c202a6cea6e0f29b598cb",
+    "url": "/static/media/solstargames.952556c1.png"
+  }
+]);
